5eaba1415a38e67a79dc0a4e9d75d5cbd39f2502
Vicky-Developer28 <vicky28.developer@gmail.com>
2026-06-17 12:55:23 +0530

    Add StatelessAPIMiddleware and reorder VisitorTrackingMiddleware

--- backend/settings.py
+++ backend/settings.py
@@ -96,14 +96,15 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
 
+    'API.middleware.StatelessAPIMiddleware',
+    'API.middleware.VisitorTrackingMiddleware',
+    
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-    'API.middleware.VisitorTrackingMiddleware',
 ]
 
 
