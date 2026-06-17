713a47689643a4f2c7a30d96bed9c7c675d736fd
Vicky-Developer28 <vicky28.developer@gmail.com>
2026-06-17 08:35:39 +0530

    Refactor logging in API views.py
    Removed duplicate logging import and added a warning log.

--- API/views.py
+++ API/views.py
@@ -1,7 +1,9 @@
+
+import logging
+
 logger.warning("API views.py loaded")
 
 import json
-import logging
 from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import get_object_or_404
