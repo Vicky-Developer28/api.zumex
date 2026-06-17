330a4092d5fd1a7dd77dea250f662d2bc34781a1
Vicky-Developer28 <vicky28.developer@gmail.com>
2026-06-17 14:43:52 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 
 CSRF_TRUSTED_ORIGINS = [
+    "http://127.0.0.1".
     "https://zumex.is-a.dev",
     "https://*.onrender.com",
     "https://zumex.onrender.com",
@@ -50,6 +51,7 @@ CSRF_TRUSTED_ORIGINS = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1".
         "https://zumex.is-a.dev",
     "https://*.onrender.com",
     "https://zumex.onrender.com",
